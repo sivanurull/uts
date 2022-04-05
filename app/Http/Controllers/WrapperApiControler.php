a4e5287dd94d33eab2d188606faf463643afc5a1
Sivaa <sivanurulasvia66444@gmail.com>
2022-04-05 09:37:39 +0700

    Update

--- app/Http/Controllers/WrapperApiControler.php
+++ app/Http/Controllers/WrapperApiControler.php
@@ -8,11 +8,11 @@ use Illuminate\Support\Facades\Http;
 class WrapperApiControler extends Controller
 {
     public function province(){
-        $json=Http::get('https://api.rajaongkir.com/starter/province?key=500f65280d54e83dc91430d8b49090a3')->json();
+        $json=Http::get('https://api.rajaongkir.com/starter/province?key=a7d4a6e78ab2f9bd57428909f8306dfa')->json();
         return response()->json($json);
     }
     public function city(){
-        $json=Http::get('https://api.rajaongkir.com/starter/city?key=500f65280d54e83dc91430d8b49090a3')->json();
+        $json=Http::get('https://api.rajaongkir.com/starter/city?key=a7d4a6e78ab2f9bd57428909f8306dfa')->json();
         return response()->json($json);
     }
     public function cost(){
@@ -21,19 +21,19 @@ class WrapperApiControler extends Controller
             "destination" => '468',
             "weight" => '1000',
             "courier"=> 'jne']), 'application/x-www-form-urlencoded')
-            ->post('https://api.rajaongkir.com/starter/cost?key=500f65280d54e83dc91430d8b49090a3')->json();
+            ->post('https://api.rajaongkir.com/starter/cost?key=a7d4a6e78ab2f9bd57428909f8306dfa')->json();
         return response()->json($json);
     }
     public function ranked(){
-        $json=Http::get('https://americas.api.riotgames.com/lor/ranked/v1/leaderboards?api_key=RGAPI-6079620c-5336-43d7-ab32-8e8d9f6f238c')->json();
+        $json=Http::get('https://americas.api.riotgames.com/lor/ranked/v1/leaderboards?api_key=RGAPI-ff34d8c3-f6d9-4b36-8ab8-23fa02e1319f')->json();
         return response()->json($json);
     }
     public function status(){
-        $json=Http::get('https://americas.api.riotgames.com/lor/status/v1/platform-data?api_key=RGAPI-6079620c-5336-43d7-ab32-8e8d9f6f238c')->json();
+        $json=Http::get('https://americas.api.riotgames.com/lor/status/v1/platform-data?api_key=RGAPI-ff34d8c3-f6d9-4b36-8ab8-23fa02e1319f')->json();
         return response()->json($json);
     }
     public function match($matchId){
-        $json=Http::get('https://americas.api.riotgames.com/lor/match/v1/matches/'.$matchId.'?api_key=RGAPI-6079620c-5336-43d7-ab32-8e8d9f6f238c')->json();
+        $json=Http::get('https://americas.api.riotgames.com/lor/match/v1/matches/'.$matchId.'?api_key=RGAPI-ff34d8c3-f6d9-4b36-8ab8-23fa02e1319f')->json();
         return response()->json($json);
     }
 }
